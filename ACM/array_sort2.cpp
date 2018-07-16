504ea2e328c1ca1456625ddbb7dac1c5ee79c9ab
dexfire <275176629@qq.com>
2018-07-16 23:55:23 +0800

    update 2018-7-16 23:55:18

--- ACM/array_sort2.cpp
+++ ACM/array_sort2.cpp
@@ -1,24 +1,27 @@
-#include<stdio.h>
+//#include<stdio.h>
+#include <iostream>
+#include <time.h>
+using namespace std;
 int abs(int x)
 {
  return x>0?x:(-x);
 }
- void swap(int *n,int *m);
-   void quicksort(int a[],int left,int right);
-int main()
-{  
-   int a[100];
-   int n,i;
-   while(scanf("%d",&n)!=0)
-   { 
-   	for(i=0;i<n;i++)
-   	scanf("%d",&a[i]);
-	quicksort(a,0,n-1);
-	for(i=0;i<n;i++)
-	printf("%d ",a[i]); 
-   }
-   return 0;
- }
+void swap(int *n,int *m);
+void quicksort(int a[],int left,int right);
+// int main()
+// {  
+//    int a[100];
+//    int n,i;
+//    while(scanf("%d",&n)!=0)
+//    { 
+//    	for(i=0;i<n;i++)
+//    	scanf("%d",&a[i]);
+// 	quicksort(a,0,n-1);
+// 	for(i=0;i<n;i++)
+// 	printf("%d ",a[i]); 
+//    }
+//    return 0;
+//  }
  void quicksort(int a[],int left,int right)
  {
  	int i,j,t;
@@ -39,14 +42,26 @@ int main()
 		 a[j]=t;
 		 quicksort(a,left,j-1);
 		 quicksort(a,j+1,right);
-		 
 	 }
  	
   } 
   void swap(int *n,int *m)
   {
-  	int s;
-  	s=*n;
+  	int s=*n;
   	*n=*m;
   	*m=s;
+  }
+
+  int main(){
+	srand(time(NULL));
+	int arr[100];
+	for (int i = 0; i < 100;i++){
+		cout<<(arr[i] = rand())<<"\t";
+	}
+	cout << endl;
+	quicksort(arr, 0, 99);
+	for (int i = 0; i < 100;i++){
+		cout<<arr[i]<<"\t";
+	}
+	system("pause");
   }
--- ACM/cable_master.cpp
+++ ACM/cable_master.cpp
@@ -1,17 +1,69 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-void sort(double arr[]){
-	
+void swap(double *a,double *b){
+	double t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void sort(double arr[],int N){
+	for (int i = 0; i < N;i++){
+		for (int j = i; j < N;j++){
+			if(arr[i]<arr[j])
+				swap(&arr[i],&arr[j]);
+		}
+	}
 }
 
 double scan(double arr[],double _min,double _max,int N,int K){
-	sort(arr);
-	double min = _min ,max = _max;
+	sort(arr,N);
+	double min = _min ,max = _max,mid;
 	int pcs = 0;
-	while(pcs<K){
-		for(int i=0;i<)
+	while(pcs!=K){
+		mid = min + (max - min) / 2;
+		//Debug cout << mid <<"\t" <<pcs<<endl;
+		pcs = 0;
+		for (int i = 0; i < N;i++){
+			pcs += (int)(arr[i] /mid);
+		}
+		if( pcs <K){
+			max = mid;
+		}else{
+			min = mid;
+		}
 	}
+
+	// 				length:  min---------mid----------max
+	// 				pieces:  MAX---------pcs----------MIN
+	//K-target pieces:  xxxxxxxoooxxxxxxxxxxxxxxxx(符合条件的长度为一个区间)
+	// 下面开始找到这个区间中最大的长度值
+	// 错误答案：超时
+	while(pcs==K){
+		// mid 每次增长1cm
+		mid += 1e-2;
+		pcs = 0;
+		for (int i = 0; i < N;i++){
+			pcs += (int)(arr[i] /mid);
+		}
+	}
+	double last_mid=mid;
+	while(mid>last_mid){
+		// mid 每次增长1cm
+		pcs = 0;
+		for (int i = 0; i < N;i++){
+			pcs += (int)(arr[i] /mid);
+		}
+		if(pcs==K)
+			last_mid = mid;
+			
+	}
+
+	// 最终结果的处理：保留2位
+	// 感觉mid应该减 个0.01 但是减了就不对了
+	mid = ((int) ((mid)  * 100)) / 100.0;	
+	return mid;
 }
 
 int main(){
@@ -23,7 +75,14 @@ int main(){
 		cin>>arr[i];
 		sum+=arr[i];
 	}
-	max = sum/N;
-	scan()
-	
+
+	//Debug
+	// int N = 4;
+	// int K= 11;
+	// double arr[] = {8.02, 7.43, 4.57, 5.39};
+	// double sum = 0;
+	// for (int i = 0; i < N;i++)
+	// 	sum += arr[i];
+	cout << setiosflags(ios::fixed) <<setprecision(2)<< scan(arr, 0, sum / N, N, K) << endl;
+	//system("pause");
 }
