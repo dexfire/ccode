b0d4c2a528fd2686d1b7bfcc2b5a60d092be605b
dexfire <275176629@qq.com>
2018-07-16 21:35:10 +0800

    added cable_master

---
+++ ACM/cable_master.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void sort(double arr[]){
+	
+}
+
+double scan(double arr[],double _min,double _max,int N,int K){
+	sort(arr);
+	double min = _min ,max = _max;
+	int pcs = 0;
+	while(pcs<K){
+		for(int i=0;i<)
+	}
+}
+
+int main(){
+	int N,K;
+	cin >> N >> K;
+	double arr[N];
+	double sum = 0;
+	for(int i=0;i<N;i++){
+		cin>>arr[i];
+		sum+=arr[i];
+	}
+	max = sum/N;
+	scan()
+	
+}
