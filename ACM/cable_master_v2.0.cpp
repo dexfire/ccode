35cab40e4ecc8b5722b98ad27796486085b2dd9c
dexfire <275176629@qq.com>
2018-07-17 17:57:19 +0800

    7-17 5:57 pm

---
+++ ACM/cable_master_v2.0.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+void buble_sort(double arr[], int N) {
+  for (int i = 0; i < N;) {
+    for (int j = i; j < N; j++) {
+    }
+  }
+}
+
+void binary_test(double arr[], double _min, double _max, int N, int K) {
+  // 注意：分的份数和 每份长度成反比
+  //  littile_inv 记录比较小的不符合要求长度【对应比较大的份数】
+  //  last_val 记录上次的复合条件的长度
+  double min = _min, mid, max = _max, last_val, last_max_inv;
+  int pcs;
+  while (true) {
+    mid = min + (max - min) / 2;
+    pcs = 0;
+    for (int i = 0; i < N; i++) {
+      pcs += (int)(arr[i] / mid);
+      if (pcs > K)
+        break;
+    }
+    if (pcs > K) {
+      min = mid;
+    } else if (pcs == K) {
+      last_val = mid;
+      break;
+    } else {
+      last_max_inv = max = mid;
+    }
+  }
+  while (last_max_inv - last_val < 0.01) {
+    mid = min + (max - min) / 2;
+    pcs = 0;
+    for (int i = 0; i < N; i++) {
+      pcs += (int)(arr[i] / mid);
+      if (pcs > K)
+        break;
+    }
+  }
+}
+
+int main() {
+  int N, K;
+  while (true) {
+    cin >> N >> K;
+    double arr[N], max;
+    for (int i = 0; i < N; i++) {
+      cin >> arr[i];
+      max += arr[i] / N;
+    }
+    binary_test(arr, N, K);
+  }
+}
