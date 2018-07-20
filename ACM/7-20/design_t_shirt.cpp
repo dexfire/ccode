51eeb259925b28466d691510db28958508d8f76b
dexfire <275176629@qq.com>
2018-07-20 21:03:23 +0800

    reorder files

--- ACM/7-20/design_t_shirt.cpp
+++ ACM/7-20/design_t_shirt.cpp
@@ -13,6 +13,7 @@ void req2idx(double ele[], double req, int count, int result[], int offset) {
             if (req == ele[j]) {
                 ele[j] = 0;
                 result[offset + i] = j;
+                break;
             }
         }
     }
@@ -20,10 +21,12 @@ void req2idx(double ele[], double req, int count, int result[], int offset) {
 
 int main() {
     while (cin >> N >> M >> K) {
-        double element[N];
-        double element_copy[N];
+        double element[M];
+        double element_copy[M];
         int result[K];
-        int req;
+        double req;
+        fill(element, element + M, 0);
+        fill(element_copy, element_copy + M, 0);
         for (int i = 0; i < N; i++) {
             element[i] = element_copy[i] = 0;
             for (int j = 0; j < M; j++) {
@@ -32,23 +35,23 @@ int main() {
             }
         }
 
-        sort(element, element + N);
+        sort(element, element + M);
 
         int c, offset = 0;
         for (int i = M - 1; i > M - K - 1; i--) {
             c = 1;
-            while (element[i] == element[i - 1]) {
+            while (i > (M - K - 2) && element[i] == element[i - 1]) {
                 i--;
                 c++;
             }
-            req2idx(element_copy, c, element[i], result, offset);
+            req2idx(element_copy, element[i], c, result, offset);
             offset += c;
         }
         sort(result, result + K);
         for (int i = K - 1; i > 0; i--) {
-            cout << result[i] << " ";
+            cout << result[i] + 1 << " ";
         }
-        cout << result[0] << endl;
+        cout << result[0] + 1 << endl;
     }
     return 0;
 }
